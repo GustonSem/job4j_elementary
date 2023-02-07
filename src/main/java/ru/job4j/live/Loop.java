04deec43d6f3bf1ad406c7833871718373f7c412
GustonSem <cytubmy1312@gmail.com>
2023-02-07 17:26:22 +0300

    6.1. Генерация конструкций в IDEA [#279134 #66468]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int row = 0; row < 10; row++) {
+            System.out.println("Мне - ");
+        }
+    }
+}
